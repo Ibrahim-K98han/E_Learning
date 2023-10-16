e4975b07702b7abd9fe242d55c6e46eafe03601f
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-10-16 12:46:55 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="css/style.css">
+    <title>E-Learning</title>
+</head>
+<body>
+   <!--Start Navigation-->
+   <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php">E-Learning</a>
+  <span class="navbar-text">Learn and Implement</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav custom-nav pl-5">
+     <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+   <!--End Navigation-->
+
+   <!-- Start Video Background -->
+   <div class="container-fluid remove-vid-marg">
+    <div class="vid-parent">
+      <video playsinline autoplay muted loop>
+        <source src="video/banvid.mp4">
+      </video>
+      <div class="vid-overlay"></div>
+    </div>
+    <div class="vid-content">
+      <h1 class="my-content">Welcome to E-Learning</h1>
+      <small class="my-content">learn and implement</small> <br>
+      <a href="#" class="btn btn-danger">Get Stated</a>
+    </div>
+   </div>
+   <!-- End Video Background -->
+
+   <!-- Start Text Banner -->
+   <div class="container-fluid bg-danger txt-banner">
+      <div class="row bottom-banner">
+        <div class="col-sm">
+            <h5><i class="fas fa-book-open mr-3"></i>100+ Online Courses</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fas fa-users mr-3"></i>Expert Instructor</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fas fa-keyboard mr-3"></i>Life Time Access</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fas fa-money mr-3"></i>Money Back Guarantee*</h5>
+        </div>
+      </div>
+   </div>
+   <!-- End Text Banner -->
+
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+</body>
+</html>
